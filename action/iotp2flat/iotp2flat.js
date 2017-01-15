2e2410b09cb820c81a4de3b357d2af037bf7a0e5
glikson <glikson@il.ibm.com>
2017-01-15 17:56:14 +0200

    Added unpacking to iotp2flat action

--- action/iotp2flat/iotp2flat.js
+++ action/iotp2flat/iotp2flat.js
@@ -9,17 +9,7 @@ Example input:
             "key": "{\"orgId\":\"9zdck8\",\"deviceType\":\"nrsim_device\",\"deviceId\":\"dev1\",\"eventType\":\"myevent\",\"format\":\"json\",\"timestamp\":\"2017-01-04T13:11:33.601+02\"}",
             "offset": 107,
             "topic": "nrsim_events",
-            "value": {
-                "d": {
-                    "velocity": 8,
-                    "type": "GPS",
-                    "name": "My Device",
-                    "location": {
-                        "latitude": 39.09,
-                        "longitude": -94.57
-                    }
-                }
-            }
+            "value": <BLOB-with-compressed-sensor-data>
         },
         { ... }
     ]
@@ -30,14 +20,14 @@ Expected output:
 	"messages": [
 		{
 			"orgId": "9zdck8",
-			"deviceType": "nrsim_device",
-			"deviceId": "dev1",
-			"eventType": "myevent",
+			"deviceType": "taxi",
+			"deviceId": "taxi1",
+			"eventType": "update",
 			"timestamp": "2017-01-04T13:11:33.601+02",
 			"payload" : {
 				"velocity": 8,
-				"type": "GPS",
-				"name": "My Device",
+				"type": "taxi",
+				"name": "taxi1",
 				"location": {
 					"latitude": 39.09,
 					"longitude": -94.57
@@ -51,6 +41,7 @@ Expected output:
 **/
 
 function main(params) {
+	zlib = require('zlib');
 	return new Promise(function(resolve, reject) {
 		if (!params.messages || !params.messages[0] || !params.messages[0].key || !params.messages[0].value) {
 			reject("Invalid arguments. Must include 'messages' JSON array with 'key' and 'value' fields");
@@ -61,16 +52,21 @@ function main(params) {
 			var msg = msgs[i];
 			console.log ("Processing MSG=" + JSON.stringify(msg));
 			var key = JSON.parse(msg.key);
-			newmsg = {
-				orgId: key.orgId,
-				deviceType: key.deviceType,
-				deviceId: key.deviceId,
-				eventType: key.eventType,
-				timestamp: key.timestamp.substr(0,19),
-				payload: msg.value["d"]
-				};
-			out.push(newmsg);
-			console.log("Result: " + newmsg);
+			var buf = Buffer.from(msg.value, "base64");
+			var sensorReadings = JSON.parse(zlib.gunzipSync(buf))["d"].sensor_data;
+			console.log ("sensor data: " + sensorReadings);
+			for (var j=0; j<sensorReadings.length; j++) {
+				newmsg = {
+					orgId: key.orgId,
+					deviceType: key.deviceType,
+					deviceId: key.deviceId,
+					eventType: key.eventType,
+					timestamp: key.timestamp.substr(0,19),
+					sensorData: sensorReadings[j]
+					};
+				out.push(newmsg);
+				console.log("result: " + newmsg);
+			}
 		}
 		resolve({ "messages": out });
 	});
